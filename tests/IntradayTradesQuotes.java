fa19921c3cf63bd55d2221a2d8dccf244a6b1d6e
jakub <jakub.rojcek@gmail.com>
2014-01-09 19:16:24 +0100

    changes 09012013

--- tests/IntradayTradesQuotes.java
+++ tests/IntradayTradesQuotes.java
@@ -13,12 +13,19 @@ public class IntradayTradesQuotes {
     public static void main(String [] args) {
         double timeStart = System.nanoTime();
         String folder = "D:\\_paper2 Nyse Event\\";
-        String ZipFileName = "AA_2.zip";
-        ArrayList qA = new ArrayList(4680);     // quotes at Ask per 5 second intervals
-        ArrayList qB = new ArrayList(4680);     // quotes at Bid per 5 second intervals
-        ArrayList n = new ArrayList(4680);      // number of trades per 5 second intervals
+        String ZipFileName = "AA_3.zip";
+        HashMap<String, ArrayList[]> tradesBursts = new HashMap<String, ArrayList[]>(); // date, trades per 5 secs
+        ArrayList qAall = new ArrayList(4680);  // quotes at Ask per 5 second intervals
+        ArrayList qBall = new ArrayList(4680);  // quotes at Bid per 5 second intervals
+        ArrayList qAnyse = new ArrayList(4680); // quotes at Ask at NYSE per 5 second intervals
+        ArrayList qBnyse = new ArrayList(4680); // quotes at Bid at NYSE per 5 second intervals
+        ArrayList nAll = new ArrayList(4680);   // number of trades per 5 second intervals
+        ArrayList nNYSE = new ArrayList(4680);  // number of trades per 5 second intervals
+        long SUMnALL = 0;                       // interval sum of trades
+        long SUMnNYSE = 0;                      // interval sum of trades
+        int interval = 5;                       // the length of the interval is 5 sec
         try {
-            ZipFile zipFile = new ZipFile(folder + "\\trades\\" + ZipFileName);
+            ZipFile zipFile = new ZipFile(folder + "\\quotes\\" + ZipFileName);
             Enumeration entries = zipFile.entries();
             ZipEntry ze = (ZipEntry) entries.nextElement();
             long size = ze.getSize();
@@ -29,19 +36,51 @@ public class IntradayTradesQuotes {
                 String line;
                 String[] lineData = null;
                 String date = null;             // is string in data at position [1]
+                int time1 = 0;                  // past 5 sec threshold
+                int time2 = 0;                  // current time
                 br.readLine();                  // this is just header
 
                 while ((line = br.readLine()) != null) {
                     lineData = line.split(",");
-                    if (!lineData[1].equals(date)){             // new date, collect data
-                        date = lineData[1];                     // new date
-                        System.out.println(date);
-                    } else  {
-                        if(lineData[8].equals("N") || lineData[8].equals("P")){     // NYSE and ARCA
-
-                        } else {
-
+                    if (!lineData[1].equals(date)){             // new unique date, collect data
+                        ArrayList[] bursts = {nAll, nNYSE};
+                        if (date != null){
+                            tradesBursts.put(date, bursts);
+                            SUMnALL = 0;
+                            SUMnNYSE = 0;
                         }
+                        if(lineData[8].equals("N") || lineData[8].equals("P")){     // NYSE and ARCA
+                            SUMnNYSE++;
+                        } else {
+                            SUMnALL++;
+                        }
+                        date = lineData[1];                     // new date
+                        time1 = Integer.parseInt(lineData[2].split(":")[2]);
+                        time2 = time1;
+                        System.out.println(date);
+                    } else {
+                        time2 = Integer.parseInt(lineData[2].split(":")[2]);
+                        if (((time2 - time1) / interval) != 0){     // new interval
+                            nAll.add(SUMnALL);
+                            nNYSE.add(SUMnNYSE);
+                            time1 = time2;
+                            SUMnALL = 0;
+                            SUMnNYSE = 0;
+                        }
+                        if(lineData[8].equals("N") || lineData[8].equals("P")){     // NYSE and ARCA
+                            SUMnNYSE++;
+                        } else {
+                            SUMnALL++;
+                        }
+                    }
+                }
+
+                if (!tradesBursts.containsKey(date) && date != null){
+                    ArrayList[] bursts = {nAll, nNYSE};
+                    if (date != null){
+                        tradesBursts.put(date, bursts);
+                        SUMnALL = 0;
+                        SUMnNYSE = 0;
                     }
                 }
                 br.close();
@@ -50,5 +89,28 @@ public class IntradayTradesQuotes {
         }  catch (IOException e) {
             e.printStackTrace();
         }
+
+        try{
+            String outputFileName = folder + "quotesTrades\\" + "AA_burst" + ".csv";
+            FileWriter writer = new FileWriter(outputFileName, true);
+
+            Iterator dates = tradesBursts.keySet().iterator();
+            String date = null;
+            int sz = 0;
+            while (dates.hasNext()){
+                date = (String) dates.next();
+                ArrayList[] bursts = tradesBursts.get(date);
+                sz = bursts[0].size();
+                for (int i = 0; i < sz; i++){
+                    writer.write(bursts[0].get(i) + "," + bursts[1].get(i) + "\r");
+                }
+            }
+            writer.close();
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            System.exit(1);
+        }
+
     }
 }
