dd0a9468cccfd8796625308c9e587d0de7c6a366
jakub <jakub.rojcek@gmail.com>
2014-01-23 17:10:23 +0100

    changes 23012013

---
+++ tests/SwtTradesQuotes.java
@@ -0,0 +1,32 @@
+/**
+ * Created by rojcek on 21.01.14.
+ */
+public class SwtTradesQuotes {
+    public static void main(String [] args) {
+
+        int[] x = {0,0,1,1,0,0,1,1,1,1,1,0,0,0,0,0};
+        for (int i = 0; i < 16; i++){
+            System.out.print(x[i] + ",");
+        }
+        System.out.println();
+        int size = 16;
+        int a = 4;
+        int[] b = x;
+        int[][] SWT = new int[4][16];
+        for (int i = 0; i < a; i++){
+            System.out.println("x for i = " + i + " is:");
+            for (int j = 0; j < size - 1; j++){
+                SWT[i][j]=b[j]+b[j+1];
+                System.out.print(SWT[i][j] + ",");
+            }
+            System.out.println();
+            System.out.println("b for i = " + i + " is:");
+            for (int j = 0; j < SWT[i].length / 2; j++){
+                b[j]=SWT[i][2 * j];
+                System.out.print(b[j] + ",");
+            }
+            System.out.println();
+            System.out.println(i);
+        }
+    }
+}
